628164b7d23e0b9d0943230728714912524f57bf
Zero <tom.new2010@gmail.com>
2019-01-02 12:05:13 +0100

    Fix basique de l'heure

--- scripts/date.js
+++ scripts/date.js
@@ -1,8 +1,7 @@
 function afficherHeure() {
     var dateCourante = new Date(); 
-    document.getElementById("heure").innerHTML = dateCourante.getHours() + ":" + (dateCourante.getMinutes()<10?'0':'') + dateCourante.getMinutes();
+    document.getElementById("heure").innerHTML = (dateCourante.getHours() + ":" + (dateCourante.getMinutes()<10?'0':'') + dateCourante.getMinutes());
     timerDate = window.setInterval("afficherHeure()", 60000);
 }
 
-window.onload = afficherHeure;
 timerDate = window.setInterval("afficherHeure()", (60 - new Date().getSeconds()) * 1000);
